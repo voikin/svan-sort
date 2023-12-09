0c1cae72533e374962c30408804db81720adfe27
voikin <voikin.a@edu.rea.ru>
2023-12-09 21:40:27 +0300

    Initial commit && added base interface

---
+++ svan-sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.CommandLine;
+
+namespace svan_sort
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var inputOption = new Option<DirectoryInfo>(
+                aliases: new[] {"--input", "-i"},
+                description: "Путь к входной папке")
+            {
+                IsRequired = true 
+            };
+
+            var outputOption = new Option<DirectoryInfo>(
+                aliases: new[] {"--out", "-o"},
+                description: "Путь к выходной папке")
+            {
+                IsRequired = true 
+            };
+
+            var rootCommand = new RootCommand("Показывает полные пути к папкам");
+            rootCommand.AddOption(inputOption);
+            rootCommand.AddOption(outputOption);
+
+            rootCommand.SetHandler((input, output) =>
+                {
+                    string fullInputPath = Path.GetFullPath(input.FullName);
+                    string fullOutputPath = Path.GetFullPath(output.FullName);
+                
+                    Console.WriteLine($"Полный путь к входной папке: {fullInputPath}");
+                    Console.WriteLine($"Полный путь к выходной папке: {fullOutputPath}");
+                },
+                inputOption, outputOption);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+    }
+}
